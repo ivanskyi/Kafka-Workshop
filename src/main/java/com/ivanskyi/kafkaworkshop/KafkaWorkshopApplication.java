bedccc266ea25c8a5d34b330e642018b3ced527d
ivanskyi <olehfromukraine@gmail.com>
2024-06-16 19:52:06 +0300

    Initial commit

---
+++ src/main/java/com/ivanskyi/kafkaworkshop/KafkaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.ivanskyi.kafkaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanskyi/kafkaworkshop/KafkaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanskyi.kafkaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
